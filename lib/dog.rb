cbeeef85166963840084d6dde5bdc3989bc2e25a
Rachel Hamby <tnthreat33@gmail.com>
2023-03-20 18:56:52 -0400

    commit tree 4f1b1b12f4b812b2ceabb00be5163e8a91000c1c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    def name=(value)
+        @name = value
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
+    
 
 end
